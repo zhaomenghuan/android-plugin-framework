873e35c3135c6daef7b2aa187fc3d429b172b8e6
zhaomenghuan <1028317108@qq.com>
2018-07-17 11:09:18 +0800

    add activity replace

--- dynamic-proxy-hook/activity-replace/src/main/java/cn/com/agree/MainActivity.java
+++ dynamic-proxy-hook/activity-replace/src/main/java/cn/com/agree/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends Activity {
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(newBase);
-        ActivityThreadHookHelper.doContextStartHook();
+        // ActivityThreadHookHelper.doContextStartHook();
         ActivityThreadHookHelper.doHandlerHook();
     }
 }
